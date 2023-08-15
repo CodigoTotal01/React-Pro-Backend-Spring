8ff03a1460e986727e13fbbeb4c2e56ba20c8ab6
CodigoTotal01 <palaciostarrillochristian@gmail.com>
2023-08-15 16:48:10 -0500

    Finalizando con tokens puros

--- src/main/java/com/codigototal/backend/usersapp/repositories/UserRepository.java
+++ src/main/java/com/codigototal/backend/usersapp/repositories/UserRepository.java
@@ -1,7 +1,16 @@
 package com.codigototal.backend.usersapp.repositories;
 
 import com.codigototal.backend.usersapp.models.entities.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    //Emeplando nombre del metodo en vez de query string
+    Optional<User>  findByUsername(String username);
+
+    @Query("select u from User u where u.username=?1")
+    Optional<User> getUserByUsername(String username);
 }
--- src/main/java/com/codigototal/backend/usersapp/services/JpaUserDetailsService.java
+++ src/main/java/com/codigototal/backend/usersapp/services/JpaUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.codigototal.backend.usersapp.services;
 
+import com.codigototal.backend.usersapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -7,23 +9,37 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-// Este se encarga de poner las reglas - es como un intermediario que da permiso a quienes quiere dejar pasar  y toma el usernaname del usaurio registrado apra esa funcion
+import java.util.Optional;
+
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
-//Aqui es ocmo si fuera parte del login serio
+
+    @Autowired
+    private UserRepository repository;
+
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!username.equals("admin")) {
+
+        Optional<com.codigototal.backend.usersapp.models.entities.User> usuarioOptional = repository.findByUsername(username);
+
+        //Si no existe el usuario - dar error
+        if (!usuarioOptional.isPresent()) {
             throw new UsernameNotFoundException(String.format("Username  %s no existe en el sistema", username));
         }
 
+
+        com.codigototal.backend.usersapp.models.entities.User user = usuarioOptional.orElseThrow();
+
+        //Si si esxiste
         List<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        return new User(username,
-                "$2a$10$DOMDxjYyfZ/e7RcBfUpzqeaCs8pLgcizuiQWXPkU35nOhZlFcE9MS",
+        return new User(user.getUsername(),
+                user.getPassword(),
                 true,
                 true,
                 true,
